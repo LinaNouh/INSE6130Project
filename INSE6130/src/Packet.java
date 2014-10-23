4a806e8d54bfe88d651bb98f9060a9e40edeed4f
Lina Nouh <lina.nouh@gmail.com>
2014-10-23 19:47:20 -0400

    Created Packet class

---
+++ INSE6130/src/Packet.java
@@ -0,0 +1,45 @@
+
+public class Packet {
+
+	float time = 0;
+	int direction = 0;
+	
+	//Default constructor   
+	public Packet(){
+		this.time = 0;
+		this.direction = 0;
+	}
+	
+	//Constructor that takes time and direction
+	public Packet(float t, int d){
+		this.time = t;
+		this.direction = d;
+	}
+	
+	//Copy constructor
+	public Packet(Packet p){
+		this.time = p.getTime();
+		this.direction = p.getDirection();
+	}
+	
+	//Accessors 
+	
+	public float getTime(){
+		return this.time;
+	}
+	
+	public int getDirection(){
+		return this.direction;
+	}
+	
+	//Mutators
+	
+	public void setTime(float t){
+		this.time = t;
+	}
+	
+	public void setDirection(int d){
+		this.direction = d;
+	}
+
+}
